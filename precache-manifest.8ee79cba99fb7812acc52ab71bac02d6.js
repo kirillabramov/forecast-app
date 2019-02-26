4c7be6e231fc7172997c84002761665274c13dc4
Кирилл Абрамов <work.kirillabramov@gmail.com>
2019-02-26 12:31:38 +0200

    Updates

---
+++ precache-manifest.8ee79cba99fb7812acc52ab71bac02d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19c49ccaa520c8cf1526",
+    "url": "/forecast-app/static/js/runtime~main.19c49cca.js"
+  },
+  {
+    "revision": "d99da8494a4848fe4117",
+    "url": "/forecast-app/static/js/main.d99da849.chunk.js"
+  },
+  {
+    "revision": "becf64fd86c9118dd614",
+    "url": "/forecast-app/static/js/1.becf64fd.chunk.js"
+  },
+  {
+    "revision": "d99da8494a4848fe4117",
+    "url": "/forecast-app/static/css/main.f88630ed.chunk.css"
+  },
+  {
+    "revision": "61e52f169e39f72aa58bd27d84ba1a30",
+    "url": "/forecast-app/index.html"
+  }
+];
